4b8dfff7dcfd89405cde1f39e92ad103e7024edf
Alex Ethier <aethier@mitre.org>
2016-09-18 20:41:30 +0000

    initial commit.

---
+++ bash_argument_parser.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Parse command line
+while [[ $# > 0 ]]
+do
+    key="$1"
+    value="$2"
+
+    # If arguments come in the form a=b
+    if [[ $1 == *'='* ]]
+    then
+        IFS='=' read -ra key_pair <<< "$1"
+        key="${key_pair[0]}"
+        value="${key_pair[1]}"
+    fi
+
+    echo "debug -- KEY IS |$key|"
+    echo "debug -- VALUE IS |$value|"
+
+    case $key in
+        --example-two)
+            EXAMPLE_TWO="$value"
+            shift
+            ;;
+        --example)
+            EXAMPLE="flag_setting"
+            ;;
+        *)
+            echo "Unknown option passed: $key"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+echo "Example Two: |$EXAMPLE_TWO|"
+echo "Example: |$EXAMPLE|"
