b53e95ee1e86d1b066837d20f69cb160a2e6d4d9
Alex Ethier <aethier@mitre.org>
2020-03-16 11:55:04 -0400

    Updated kinit script.

--- kinit_autologin.sh
+++ kinit_autologin.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+# In case the calling process spams autorestarts have a sleep up front to prevent spamming the krb server.
+sleep 1
+
 USER="$1"
 LOGIN_FILE="$2"
+EXPIRES="0"
 
 while true; do
-  echo "[$(date)] Logging in"
-  cat $LOGIN_FILE | kinit --password-file=STDIN $USER
 
   TICKETS=`klist --json`
   if [ "$TICKETS" -eq "{}" ]; then
-    echo "Ticket allocation failed.  Short sleep"
-    sleep 60
+    echo "No ticket found."
   else
-    echo "Received ticket"
+    echo "Existing ticket"
     echo "$TICKETS"
-    sleep 3600
+    # Assumes we only ever have one ticket, or at least the first one is the one we care about
+    EXPIRES=`klist --json | jq -r '.tickets[0]["Expires"]' | xargs -I {} date --date="{}" +"%s" | xargs -I {} /bin/bash -c 'echo "$(({} - $(date +%s)))"'`
+    echo "Expires in $EXPIRES"
+  fi
+
+  if [ "$EXPIRES" -lt "5000" ]; then
+    echo "[$(date)] Logging in with $USER and credentials $LOGIN_FILE"
+    cat $LOGIN_FILE | kinit --password-file=STDIN $USER
+  else
+    echo "Valid ticket exits, long sleep"
+    sleep 2000 # If initial tests work we should dynamically sleep based on the remaining expiration time
+  fi
+
+  TICKETS=`klist --json`
+  if [ "$TICKETS" -eq "{}" ]; then
+    echo "Failed getting ticket, short sleep"
+    sleep 60
   fi
 done
