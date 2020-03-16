1f8b42eebb43b0a428547cb16ffe987bbda5ccb1
Alex Ethier <aethier@mitre.org>
2020-03-16 13:24:15 -0400

    Updated kinit autologin to use issued date rather than expired time.

--- kinit_autologin.sh
+++ kinit_autologin.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# In case the calling process spams autorestarts have a sleep up front to prevent spamming the krb server.
-sleep 1
-
 USER="$1"
 LOGIN_FILE="$2"
 EXPIRES="0"
 
 while true; do
+
+  # In case the calling process spams autorestarts have a sleep up front to prevent spamming the krb server.
+  sleep 1
 
   TICKETS=`klist --json`
   if [ "$TICKETS" -eq "{}" ]; then
@@ -17,16 +17,28 @@ while true; do
     echo "$TICKETS"
     # Assumes we only ever have one ticket, or at least the first one is the one we care about
     EXPIRES=`klist --json | jq -r '.tickets[0]["Expires"]' | xargs -I {} date --date="{}" +"%s" | xargs -I {} /bin/bash -c 'echo "$(({} - $(date +%s)))"'`
-    echo "Expires in $EXPIRES"
+    ISSUED=`klist --json | jq -r '.tickets[0]["Issued"]' | xargs -I {} date --date="{}" +"%s" | xargs -I {} /bin/bash -c 'echo "$(($(date +%s) - {}))"'`
+    echo "Issued ${ISSUED}s ago, expires in ${EXPIRES}s"
   fi
 
-  if [ "$EXPIRES" -lt "5000" ]; then
+  if [ "$ISSUED" -gt "3000" ]; then
     echo "[$(date)] Logging in with $USER and credentials $LOGIN_FILE"
     cat $LOGIN_FILE | kinit --password-file=STDIN $USER
   else
-    echo "Valid ticket exits, long sleep"
-    sleep 2000 # If initial tests work we should dynamically sleep based on the remaining expiration time
+    SLEEP_TIME=$((3005 - ${ISSUED}))
+    echo "Valid ticket found, sleeping for $SLEEP_TIME"
+    sleep $SLEEP_TIME
   fi
+
+  # For w.e. reason the expiration does not work.
+  # Instead wait 50 minutes after the issue time
+  #if [ "$EXPIRES" -lt "5000" ]; then
+  #  echo "[$(date)] Logging in with $USER and credentials $LOGIN_FILE"
+  #  cat $LOGIN_FILE | kinit --password-file=STDIN $USER
+  #else
+  #  echo "Valid ticket exits, long sleep"
+  #  sleep 2000 # If initial tests work we should dynamically sleep based on the remaining expiration time
+  #fi
 
   TICKETS=`klist --json`
   if [ "$TICKETS" -eq "{}" ]; then
